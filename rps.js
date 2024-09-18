c48b6263ea47e9a43c76bcde4f757a2493db866f
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-09-18 18:38:11 +0545

    first commit

--- rps.js
+++ rps.js
@@ -71,3 +71,18 @@ let score=JSON.parse(localStorage.getItem('score'));
       }
       return computerMove;
     }
+
+    let isAutoPlaying=false;
+    let intervalId;
+    function autoPlay(){
+      if(!isAutoPlaying){
+        intervalId=setInterval(function(){
+          const playerMove= pickComputerMove();
+          playGame(playerMove);
+        }, 1000);
+        isAutoPlaying=true;
+    }else{
+      clearInterval(intervalId);
+      isAutoPlaying=false;
+    }
+    }
