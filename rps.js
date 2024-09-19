0d2fb272c97c9930a9b030ebbd033b4abc102486
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-09-19 09:35:10 +0545

    key-presses

--- rps.js
+++ rps.js
@@ -6,7 +6,30 @@ let score=JSON.parse(localStorage.getItem('score'));
         ties: 0
       }
     }
-    updateScore();    
+    updateScore(); 
+    
+    document.querySelector('.js-rock-button').addEventListener('click', ()=>{
+      playGame('rock');
+    });
+    document.querySelector('.js-paper-button').addEventListener('click', ()=>{
+      playGame('paper');
+    });
+    document.querySelector('.js-scissors-button').addEventListener('click', ()=>{
+      playGame('scissors');
+    });
+
+
+    document.body.addEventListener('keydown', (event)=>{
+      if(event.key==='r'){
+        playGame('rock');
+      }else if(event.key==='p'){
+        playGame('paper');
+      }else if(event.key==='s'){
+        playGame('scissors');
+      }else{
+        alert('Enter valid keys');
+      }
+    });
      function playGame(playerMove){
       const computerMove=pickComputerMove();
   let result='';
@@ -74,9 +97,12 @@ let score=JSON.parse(localStorage.getItem('score'));
 
     let isAutoPlaying=false;
     let intervalId;
+
+  
+
     function autoPlay(){
       if(!isAutoPlaying){
-        intervalId=setInterval(function(){
+        intervalId=setInterval(()=>{
           const playerMove= pickComputerMove();
           playGame(playerMove);
         }, 1000);
